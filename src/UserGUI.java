bf015a8a3d15166c5ad2ed7fa438473b9f150160
MrNoName <comeheremybuddy>
2017-04-26 22:56:11 +0300

    Yes I did

--- src/UserGUI.java
+++ src/UserGUI.java
@@ -1,3 +1,5 @@
+//I changed shit
+
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.GridBagConstraints;
@@ -47,64 +49,70 @@ public class UserGUI implements ActionListener {
 
     public void go() {
         panel = new JPanel();
-        GridBagLayout gbl_panel = new GridBagLayout();
-        gbl_panel.columnWeights = new double[]{0.0, 0.0};
-        panel.setLayout(gbl_panel);
         panel.setBackground(Color.ORANGE);
 
         frame = new JFrame("Create a new account");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.getContentPane().add(BorderLayout.CENTER, panel);
-        
-        lblUsername = new JLabel("Username:");
-        GridBagConstraints gbc_lblUsername = new GridBagConstraints();
-        gbc_lblUsername.insets = new Insets(0, 0, 5, 5);
-        gbc_lblUsername.anchor = GridBagConstraints.WEST;
-        gbc_lblUsername.gridx = 0;
-        gbc_lblUsername.gridy = 1;
-        panel.add(lblUsername, gbc_lblUsername);
+        GridBagLayout gbl_panel = new GridBagLayout();
+        gbl_panel.columnWidths = new int[]{51, 90, 86, 0};
+        gbl_panel.rowHeights = new int[]{34, 20, 14, 40, 20, 20, 0, 0};
+        gbl_panel.columnWeights = new double[]{0.0, 0.0, 0.0, Double.MIN_VALUE};
+        gbl_panel.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+        panel.setLayout(gbl_panel);
         
         textField_2 = new JTextField();
         textField_2.setColumns(10);
         GridBagConstraints gbc_textField_2 = new GridBagConstraints();
         gbc_textField_2.anchor = GridBagConstraints.WEST;
         gbc_textField_2.insets = new Insets(0, 0, 5, 0);
-        gbc_textField_2.gridx = 1;
+        gbc_textField_2.gridx = 2;
         gbc_textField_2.gridy = 1;
         panel.add(textField_2, gbc_textField_2);
         
+        lblUsername = new JLabel("Username:");
+        GridBagConstraints gbc_lblUsername = new GridBagConstraints();
+        gbc_lblUsername.anchor = GridBagConstraints.WEST;
+        gbc_lblUsername.insets = new Insets(0, 0, 5, 5);
+        gbc_lblUsername.gridx = 1;
+        gbc_lblUsername.gridy = 2;
+        panel.add(lblUsername, gbc_lblUsername);
+        
         lblPassword = new JLabel("Password:");
+        lblPassword.setEnabled(true);
         GridBagConstraints gbc_lblPassword = new GridBagConstraints();
         gbc_lblPassword.anchor = GridBagConstraints.WEST;
         gbc_lblPassword.insets = new Insets(0, 0, 5, 5);
-        gbc_lblPassword.gridx = 0;
-        gbc_lblPassword.gridy = 2;
+        gbc_lblPassword.gridx = 1;
+        gbc_lblPassword.gridy = 4;
         panel.add(lblPassword, gbc_lblPassword);
         
         textField_1 = new JTextField();
+        textField_1.setEnabled(true);
         textField_1.setColumns(10);
         GridBagConstraints gbc_textField_1 = new GridBagConstraints();
         gbc_textField_1.anchor = GridBagConstraints.WEST;
         gbc_textField_1.insets = new Insets(0, 0, 5, 0);
-        gbc_textField_1.gridx = 1;
-        gbc_textField_1.gridy = 2;
+        gbc_textField_1.gridx = 2;
+        gbc_textField_1.gridy = 4;
         panel.add(textField_1, gbc_textField_1);
+        
+        textField = new JTextField();
+        textField.setColumns(10);
+        GridBagConstraints gbc_textField = new GridBagConstraints();
+        gbc_textField.insets = new Insets(0, 0, 5, 0);
+        gbc_textField.anchor = GridBagConstraints.WEST;
+        gbc_textField.gridx = 2;
+        gbc_textField.gridy = 5;
+        panel.add(textField, gbc_textField);
         
         lblConfirmPassword = new JLabel("Confirm Password:");
         GridBagConstraints gbc_lblConfirmPassword = new GridBagConstraints();
         gbc_lblConfirmPassword.anchor = GridBagConstraints.WEST;
         gbc_lblConfirmPassword.insets = new Insets(0, 0, 0, 5);
-        gbc_lblConfirmPassword.gridx = 0;
-        gbc_lblConfirmPassword.gridy = 3;
+        gbc_lblConfirmPassword.gridx = 1;
+        gbc_lblConfirmPassword.gridy = 6;
         panel.add(lblConfirmPassword, gbc_lblConfirmPassword);
-        
-        textField = new JTextField();
-        textField.setColumns(10);
-        GridBagConstraints gbc_textField = new GridBagConstraints();
-        gbc_textField.anchor = GridBagConstraints.WEST;
-        gbc_textField.gridx = 1;
-        gbc_textField.gridy = 3;
-        panel.add(textField, gbc_textField);
 
         JButton createAccount = new JButton("Create this account");
         frame.getContentPane().add(BorderLayout.SOUTH, createAccount);

32775fd35ca1acbc59cf9fd32c473b8233341036
OFOL <outforonelook@gmail.com>
2017-04-26 14:47:46 -0400

    WindowBuilder compatibility
    Updated text fields and text boxes to be compatible with WindowBuilder

--- src/UserGUI.java
+++ src/UserGUI.java
@@ -17,26 +17,28 @@ import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
+import java.awt.Insets;
 
 public class UserGUI implements ActionListener {
 
     JFrame          frame;
     JPanel          panel;
-    JTextField      username;
     JPasswordField  password;
     JPasswordField  confirmPassword;
     JLabel          warningLabel;
+    private JLabel lblUsername;
+    private JLabel lblPassword;
+    private JLabel lblConfirmPassword;
+    private JTextField textField_2;
+    private JTextField textField_1;
+    private JTextField textField;
 
     public static void main(String[] args) {
         SwingUtilities.invokeLater(new Runnable() {
             @Override
             public void run() {
-                try {
-                    UIManager.setLookAndFeel(UIManager
-                            .getSystemLookAndFeelClassName());
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
+                try { UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName()); } 
+                catch (Exception e) { e.printStackTrace(); }
                 UserGUI window = new UserGUI();
                 window.go();
             }
@@ -45,36 +47,64 @@ public class UserGUI implements ActionListener {
 
     public void go() {
         panel = new JPanel();
-        panel.setLayout(new GridBagLayout());
+        GridBagLayout gbl_panel = new GridBagLayout();
+        gbl_panel.columnWeights = new double[]{0.0, 0.0};
+        panel.setLayout(gbl_panel);
         panel.setBackground(Color.ORANGE);
 
         frame = new JFrame("Create a new account");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.getContentPane().add(BorderLayout.CENTER, panel);
-
-        JLabel userLabel = new JLabel("Username:");
-        JLabel passwordLabel = new JLabel("Password:");
-        JLabel confirmPasswordLabel = new JLabel("Confirm Password:");
-        username = new JTextField(15);
-        password = new JPasswordField(15);
-        confirmPassword = new JPasswordField(15);
-
-        GridBagConstraints right = new GridBagConstraints();
-        right.anchor = GridBagConstraints.WEST;
-        GridBagConstraints left = new GridBagConstraints();
-        left.anchor = GridBagConstraints.EAST;
-
-        right.weightx = (int) 2;
-        right.fill = GridBagConstraints.REMAINDER;
-        right.gridwidth = GridBagConstraints.REMAINDER;
-        // actual GUI
-
-        panel.add(userLabel, left);
-        panel.add(username, right);
-        panel.add(passwordLabel, left);
-        panel.add(password, right);
-        panel.add(confirmPasswordLabel, left);
-        panel.add(confirmPassword, right);
+        
+        lblUsername = new JLabel("Username:");
+        GridBagConstraints gbc_lblUsername = new GridBagConstraints();
+        gbc_lblUsername.insets = new Insets(0, 0, 5, 5);
+        gbc_lblUsername.anchor = GridBagConstraints.WEST;
+        gbc_lblUsername.gridx = 0;
+        gbc_lblUsername.gridy = 1;
+        panel.add(lblUsername, gbc_lblUsername);
+        
+        textField_2 = new JTextField();
+        textField_2.setColumns(10);
+        GridBagConstraints gbc_textField_2 = new GridBagConstraints();
+        gbc_textField_2.anchor = GridBagConstraints.WEST;
+        gbc_textField_2.insets = new Insets(0, 0, 5, 0);
+        gbc_textField_2.gridx = 1;
+        gbc_textField_2.gridy = 1;
+        panel.add(textField_2, gbc_textField_2);
+        
+        lblPassword = new JLabel("Password:");
+        GridBagConstraints gbc_lblPassword = new GridBagConstraints();
+        gbc_lblPassword.anchor = GridBagConstraints.WEST;
+        gbc_lblPassword.insets = new Insets(0, 0, 5, 5);
+        gbc_lblPassword.gridx = 0;
+        gbc_lblPassword.gridy = 2;
+        panel.add(lblPassword, gbc_lblPassword);
+        
+        textField_1 = new JTextField();
+        textField_1.setColumns(10);
+        GridBagConstraints gbc_textField_1 = new GridBagConstraints();
+        gbc_textField_1.anchor = GridBagConstraints.WEST;
+        gbc_textField_1.insets = new Insets(0, 0, 5, 0);
+        gbc_textField_1.gridx = 1;
+        gbc_textField_1.gridy = 2;
+        panel.add(textField_1, gbc_textField_1);
+        
+        lblConfirmPassword = new JLabel("Confirm Password:");
+        GridBagConstraints gbc_lblConfirmPassword = new GridBagConstraints();
+        gbc_lblConfirmPassword.anchor = GridBagConstraints.WEST;
+        gbc_lblConfirmPassword.insets = new Insets(0, 0, 0, 5);
+        gbc_lblConfirmPassword.gridx = 0;
+        gbc_lblConfirmPassword.gridy = 3;
+        panel.add(lblConfirmPassword, gbc_lblConfirmPassword);
+        
+        textField = new JTextField();
+        textField.setColumns(10);
+        GridBagConstraints gbc_textField = new GridBagConstraints();
+        gbc_textField.anchor = GridBagConstraints.WEST;
+        gbc_textField.gridx = 1;
+        gbc_textField.gridy = 3;
+        panel.add(textField, gbc_textField);
 
         JButton createAccount = new JButton("Create this account");
         frame.getContentPane().add(BorderLayout.SOUTH, createAccount);
@@ -86,27 +116,18 @@ public class UserGUI implements ActionListener {
         frame.setSize(300, 250);
         frame.setVisible(true);
     }
-
-    // This is where the problem is!
+    
     public void actionPerformed(ActionEvent event) {
-        if (!(Arrays.equals(password.getPassword(),
-                confirmPassword.getPassword()))) {
-            warningLabel
-                    .setText("Your passwords do not match! Please try again.");
-        } else if (password.getPassword().length < 1) {
-            warningLabel
-                    .setText("That password is not long enough! Please try again!");
-        } else {
+        if (!(Arrays.equals(password.getPassword(), confirmPassword.getPassword()))) { warningLabel.setText("Your passwords do not match! Please try again."); } 
+        else if (password.getPassword().length < 1) { warningLabel.setText("That password is not long enough! Please try again!");}
+        else {
             try {
             	User user = new User();
-                BufferedWriter writer = new BufferedWriter(new FileWriter(
-                        "nuserInfo.txt"));
+                BufferedWriter writer = new BufferedWriter(new FileWriter("nuserInfo.txt"));
                 writer.write(username.getText() + "/" + new String(password.getPassword()) + "/" + user.getID());
                 user.setName(username.getText());
-                writer.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+                writer.close(); }
+            catch (IOException e) { e.printStackTrace(); }
         }
     }
 }
